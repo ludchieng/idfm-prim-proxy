c0b4584f2c50b77373e2fd7b268e0d1d1a96b5a5
ludwig.chieng <ludwig.chieng@octo.com>
2022-09-06 18:33:56 +0200

    Add starting blocks

---
+++ app.js
@@ -0,0 +1,29 @@
+import Fastify from "fastify";
+import fetch from "node-fetch";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/stop-monitoring", async function (req, res) {
+  console.log(req.url)
+  const data = await fetch(
+    process.env.PRIM_IDFM_API_URL + req.url,
+    {
+      headers: {
+        apiKey: process.env.PRIM_IDFM_API_KEY,
+      },
+    }
+  );
+  res.send(await data.json());
+});
+
+fastify.listen({ port: 3000 }, function (err, address) {
+  if (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+});
